faed58087fdab546f5eaf00adf4a3b40023a1767
kadzahk <juan427lol@gmail.com>
2022-05-05 11:47:30 -0500

    number of and the list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    a = len(argv)
+    if a == 1:
+        print(f"{a - 1} arguments.")
+    elif a == 2:
+        print(f"{a - 1} argument:")
+    else:
+        print(f"{a - 1} arguments:")
+    for x in range(1, a):
+        print(f"{x}: {argv[x]}")
