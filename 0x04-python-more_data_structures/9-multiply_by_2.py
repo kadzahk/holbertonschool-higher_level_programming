858e735a6d34c9f9d05568a9d7ea04e9eeae3ddf
kadzahk <juan427lol@gmail.com>
2022-05-10 13:17:18 -0500

    returns a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     dictionary_multi_by_2 = a_dictionary.copy()
-    return [list(map(lambda x: x * 2, b)) for b in dictionary_multi_by_2]
+    for i in dictionary_multi_by_2:
+        dictionary_multi_by_2[i] *= 2
+    return dictionary_multi_by_2
