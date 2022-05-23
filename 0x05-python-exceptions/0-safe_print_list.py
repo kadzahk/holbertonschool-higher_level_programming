dfea8353f93d01b0fc3d67d22531e53275535087
kadzahk <juan427lol@gmail.com>
2022-05-23 12:13:47 -0500

    print a list with a except of an index error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        cc = 0 
+        cc = 0
         while cc < x:
             print(my_list[cc], end="")
-            cc +=1
+            cc += 1
         print()
     except IndexError:
         print()
