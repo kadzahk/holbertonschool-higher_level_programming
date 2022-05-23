2ef9c277c6c230dc99355761ad3d86b8ddaa461d
kadzahk <juan427lol@gmail.com>
2022-05-23 12:39:09 -0500

    prints an integer and write a standar error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as error:
+        stderr.write('Exception: {}\n'.format(error))
+        return False
+    return True
