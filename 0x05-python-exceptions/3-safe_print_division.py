a493ca3fa15eb8f5a74291f7acf76a296290ba9e
kadzahk <juan427lol@gmail.com>
2022-05-23 12:33:44 -0500

    div 2 values with the except of ZeroDivisionError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,9 @@
 def safe_print_division(a, b):
     try:
         divv = a / b
+        print("Inside result: {:.1f}".format(divv))
     except ZeroDivisionError:
         divv = None
+        print("Inside result: {}".format(divv))
     finally:
-        print("Inside divv: {}".format(divv))
-    return divv
+        return divv
