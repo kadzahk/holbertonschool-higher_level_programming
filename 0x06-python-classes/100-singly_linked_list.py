ae73f0463287f9be7c2937e28e33c0e6616d1995
kadzahk <juan427lol@gmail.com>
2022-05-24 13:22:02 -0500

    making linked lists in python, then we insert values in there

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+
+class Node:
+
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+
+    def __init__(self):
+        self.__head = None
+
+    def sorted_insert(self, value):
+        new_node = Node(value, None)
+        temp = self.__head
+        if not self.__head:
+            self.__head = new_node
+            return
+        if temp.data > value:
+            new_node.next_node = temp
+            self.__head = new_node
+            return
+        while temp.next_node is not None:
+            if temp.next_node.data > value:
+                break
+            temp = temp.next_node
+        new_node.next_node = temp.next_node
+        temp.next_node = new_node
+        return
+
+    def __str__(self):
+        list_values = ''
+        temp = self.__head
+        while temp is not None:
+            list_values += str(temp.data)
+            if temp.next_node is not None:
+                list_values += '\n'
+            temp = temp.next_node
+        return list_values
