400980e5bf096ae04bc7a05a74568cb5f9bd6595
kadzahk <juan427lol@gmail.com>
2022-05-24 20:59:06 -0500

    compare 2 squares

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+""" class square """
 
 
 class Square:
+    """start of a class with setting size """
 
     def __init__(self, size=0):
+        """setting size"""
         self.size = size
 
     @property
@@ -20,22 +23,29 @@ class Square:
             raise ValueError("size must be >= 0")
 
     def area(self):
+        """return size **2"""
         return self.__size ** 2
 
     def __lt__(self, other):
+        """verify situation"""
         return self.area() < other.area()
 
     def __le__(self, other):
+        """verify situation"""
         return self.area() <= other.area()
 
     def __eq__(self, other):
+        """verify situation"""
         return self.area() == other.area()
 
     def __ne__(self, other):
+        """verify situation"""
         return self.area() != other.area()
 
     def __gt__(self, other):
+        """verify situation"""
         return self.area() > other.area()
 
     def __ge__(self, other):
+        """verify situation"""
         return self.area() >= other.area()
