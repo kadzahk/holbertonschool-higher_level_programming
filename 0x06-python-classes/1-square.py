bb0923ac4223ba5579c9a13980fa4beb26bdb3ae
kadzahk <juan427lol@gmail.com>
2022-05-24 19:46:42 -0500

    based on 0-square.py Private instance attribute: size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
+""" This module contains an Square class """
 
 class Square:
+    """ This a square class being crated """
     def __init__(self, size):
+        """ Here we asing a private instance attribute callled size """
         self.__size = size
