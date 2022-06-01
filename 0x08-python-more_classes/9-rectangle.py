ea9e217977134253e19fd09e2e5898b74ca94e1c
kadzahk <juan427lol@gmail.com>
2022-06-01 00:02:29 -0500

    Class method that returns a new Rectangle instance

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -87,5 +87,5 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        """ Function that returns a new Rectangle instance with width == height == size"""
+        """ Function that returns a new Rectangle instance """
         return Rectangle(size, size)
