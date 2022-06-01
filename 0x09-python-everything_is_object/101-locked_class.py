ea28f181302e6bd16566cb26c8237b4fc8cd52a2
kadzahk <juan427lol@gmail.com>
2022-06-01 13:41:04 -0500

    a class with no class or object attribute

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" This is the LockedClass module """
+
+
+class LockedClass:
+    """ Special class that prevents the user from dynamically creating new instance
+    attributes, except if the new instance attribute is called first_name """
+
+    __slots__ = ['first_name']
+
+    def __init__(self, first_name=""):
+        """initializes attribute first_name"""
+        self.first_name = first_name
