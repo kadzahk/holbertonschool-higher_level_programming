9b811bc127ce0dca4270bde17c8511417d3283cb
kadzahk <juan427lol@gmail.com>
2022-06-07 14:17:22 -0500

    raises an Exception with the message area() is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module of BaseGeometry"""
+
+
+class BaseGeometry:
+    """ class BaseGeometry"""
+
+    def area(self):
+        """raises an Exception"""
+        raise Exception('area() is not implemented')
