25bac8c9e93959dd4e362f2ad60532c0c1ab73ec
kadzahk <juan427lol@gmail.com>
2022-06-08 21:19:16 -0500

    add all arguments to a list and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,9 +7,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
-    text = load_from_json_file("add_item.json")
+    txt = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    text = list()
+    txt = list()
 for argument in sys.argv[1:]:
-    text.append(argument)
-save_to_json_file(text, "add_item.json")
+    txt.append(argument)
+save_to_json_file(txt, "add_item.json")
