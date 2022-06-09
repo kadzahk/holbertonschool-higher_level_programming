de1cd2867ea1f3139a77da8f857d2f2d98159ece
kadzahk <juan427lol@gmail.com>
2022-06-08 20:06:01 -0500

    reads a text file (UTF8) and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ module of read_file"""
 
+
 def read_file(filename=""):
     """reads a text file and prints it"""
-    with open(filename, 't') as t:
-        file_text = t.read()
+    with open(filename, 'r') as r:
+        file_text = r.read()
         print(file_text, end='')
