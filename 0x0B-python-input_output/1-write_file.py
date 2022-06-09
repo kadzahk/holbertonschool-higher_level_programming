9c7e84e439c4be0fbe4aa66b07353313ed7153d7
kadzahk <juan427lol@gmail.com>
2022-06-08 20:25:00 -0500

    returns the number of characters written

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def number_of_lines(filename=""):
     """returns the number of lines of a text file"""
-    with open(filename, 'r') as f:
-        return len(f.readlines())
+    with open(filename, 'r') as r:
+        return len(r.readlines())
