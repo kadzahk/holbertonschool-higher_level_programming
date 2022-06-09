503ffcf0302fc0b2b2071232c3b53b7cb6dd6498
kadzahk <juan427lol@gmail.com>
2022-06-08 21:24:19 -0500

    add all arguments to a list and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 def add_items():
     """function that adds all arguments to a Python list
     and then saves them to a file"""
+
     try:
         python_list = load_from_json_file('add_item.json')
     except:
@@ -19,6 +20,7 @@ def add_items():
         python_list.append(sys.argv[x])
 
     save_to_json_file(python_list, 'add_item.json')
+
 
 if __name__ == '__main__':
     add_items()
