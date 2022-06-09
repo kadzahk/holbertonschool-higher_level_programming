9375ec17e977d6fa9476880d314306948b48def5
kadzahk <juan427lol@gmail.com>
2022-06-08 21:43:00 -0500

    returns the dictionary description with simple data structure

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" module of class_to_json"""
+
+
+def class_to_json(obj):
+    """ returns the dictionary description with simple data structure
+    for JSON serialization of an object"""
+    return obj.__dict__
