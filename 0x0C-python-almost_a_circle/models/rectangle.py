e48f869415f9ff77f8c543649ea9befa2db9d321
kadzahk <juan427lol@gmail.com>
2022-06-14 19:51:03 -0500

    rectangle form completed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 """rectangle module"""
-
 from models.base import Base
 
 
 class Rectangle(Base):
     """class Rectangle inherits from base"""
-    key_dict = {'id': 'id', 'width': '_Rectangle__width',
-                'height': '_Rectangle__height', 'x':
-                '_Rectangle__x', 'y':
-                '_Rectangle__y'}
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """method __init__ Initialization a Rectangle"""
@@ -19,18 +14,71 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
+    @property
+    def width(self):
+        """getter for width, retrieves width value"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter for width, validates the value assignment for width"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format("width"))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format("width"))
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter for height, retrieves height value"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height, validates the value assignment for height"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format("height"))
+        if value <= 0:
+            raise ValueError("{} must be > 0".format("height"))
+        self.__height = value
+
+    @property
+    def x(self):
+        """getter for x, retrieves x value"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """setter for x, validates the value assignment for x"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format("x"))
+        if value < 0:
+            raise ValueError("{} must be >= 0".format("x"))
+        self.__x = value
+
+    @property
+    def y(self):
+        """getter for y, retrieves y value"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter for y, validates the value assignment for y"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format("y"))
+        if value < 0:
+            raise ValueError("{} must be >= 0".format("y"))
+        self.__y = value
+
     def update(self, *args, **kwargs):
         """method update """
-        key_list = ['id', '_Rectangle__width', '_Rectangle__height',
-                    '_Rectangle__x', '_Rectangle__y']
-        key_dict = {'id': 'id', 'width': '_Rectangle__width', 'height':
-                    '_Rectangle__height', 'x': '_Rectangle__x', 'y':
-                    '_Rectangle__y'}
-        for index, el in enumerate(args):
-            self.__dict__[key_list[index]] = el
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__dict__[key_dict[key]] = val
+        a_list = ["id", "width", "height", "x", "y"]
+        if args is not None and len(args) > 0 and len(args) <= 5:
+            for i, arg in enumerate(args):
+                setattr(self, a_list[i], arg)
+        if kwargs is not None and len(kwargs) > 0 and len(kwargs) <= 5:
+            for name, value in kwargs.items():
+                setattr(self, name, value)
 
     def __str__(self):
         """method that """
@@ -40,10 +88,28 @@ class Rectangle(Base):
 
     def area(self):
         """public_method area of rectangle """
-        return self.__width * self.__height
+        return self.width * self.height
 
     def display(self):
         """public method display self prints in stdout """
-        print("\n" * (self.__y), end="")
-        for i in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
+        pattern = ""
+        if self.width is 0 or self.height is 0:
+            print(pattern)
+        else:
+            for k in range(self.y):
+                pattern += '\n'
+            for j in range(self.height):
+                for h in range(self.x):
+                    pattern += ' '
+                for i in range(self.width):
+                    pattern += '#'
+                if j is not (self.height - 1):
+                    pattern += '\n'
+            print(pattern)
+
+    def to_dictionary(self):
+        """function that returns the dictionary representation
+        of a Rectangle
+        object/instance"""
+        return {"id": self.id, "width": self.width, "height": self.height,
+                "x": self.x, "y": self.y}
