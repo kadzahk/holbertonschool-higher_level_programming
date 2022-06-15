36d8f0cb441a75c670076c318a8e6e7ff5dfc75d
kadzahk <juan427lol@gmail.com>
2022-06-14 19:28:11 -0500

    square form completed

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Define Rectangle Class"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Module Representation of Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialization a Square"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """module Square size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """module Square size setter """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """string represation of square"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
+
+    def update(self, *args, **kwargs):
+        """update the data of the square with a kwargs"""
+        if len(args):
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if hasattr(self, key) is True:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        """retrun dictonary a dictionary of id, size, x and y"""
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
