f4994cb0f7dcf331c04da2e02293fe176f0938e7
kadzahk <juan427lol@gmail.com>
2022-08-16 13:33:09 -0500

    prints the alphabet in uppercase

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,21 @@
 const square = require('./5-square');
 
 class Square extends square {
+  constructor (size) {
+    super(size, size);
+  }
+
   charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
+    let myStr = '';
+    for (let i = 0; i < this.width; i++) {
+      if (c === undefined) {
+        myStr += 'X';
+      } else {
+        myStr += c;
+      }
     }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+    for (let j = 0; j < this.height; j++) {
+      console.log(myStr);
     }
   }
 }
