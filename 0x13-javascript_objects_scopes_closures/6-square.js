37d7f8416a58e5432926e5235aadfb95ff87fadb
kadzahk <juan427lol@gmail.com>
2022-08-16 13:36:00 -0500

    charPrint prints the rectangle using the character c

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,21 @@
 #!/usr/bin/node
 
-const Square5 = require('./5-square.js');
-
-class Square extends Square5 {
-  charPrint (c = 'X') {
-    const character = c;
-    const { width, height } = this;
-    const shape = `${character.repeat(width)}\n`.repeat(height).slice(0, -1);
-    console.log(shape);
+module.exports = class Square extends require('./5-square') {
+  constructor (size) {
+    super(size, size);
   }
-}
 
-module.exports = Square;
+  charPrint (c) {
+    let myStr = '';
+    for (let i = 0; i < this.width; i++) {
+      if (c === undefined) {
+        myStr += 'X';
+      } else {
+        myStr += c;
+      }
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(myStr);
+    }
+  }
+};
