177add1d06bbe19f548f7bb699ade7de66b1be9e
kadzahk <juan427lol@gmail.com>
2022-08-17 12:06:48 -0500

    prints all characters of a Star Wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const movieNumber = process.argv.slice(2);
+const myArray = process.argv.slice(2);
 const request = require('request');
 
-if (Number.isInteger(parseInt(movieNumber[0]))) {
-  request('http://swapi.co/api/films/' + movieNumber[0], function (error, response, body) {
+if (Number.isInteger(parseInt(myArray[0]))) {
+  request('http://swapi.co/api/films/' + myArray[0], function (error, response, body) {
     if (error) console.error('error:', error);
     JSON.parse(body).characters.forEach(link => request(link, (error, response, body) => {
       if (error) console.error('error:', error);
